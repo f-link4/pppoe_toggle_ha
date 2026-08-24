99757091abc6626e6e85bf1a8fcd3ce5e7b5eba6
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-08-24 11:50:53 +0200

    Simplify installation script for PPPoE Toggle HA
    Removed redundant checks for existence of configuration files and streamlined installation commands.

--- install.sh
+++ install.sh
@@ -14,16 +14,13 @@ TMPDIR=$(mktemp -d)
 cd $TMPDIR || exit 1
 
 echo "Downloading PPPoE Toggle HA from GitHub..."
-
 fetch https://github.com/f-link4/pppoe_toggle_ha/archive/main.tar.gz
-
 if [ $? -ne 0 ]; then
     echo "Failed to download from GitHub"
     exit 1
 fi
 
 tar -xzf main.tar.gz
-
 if [ -d "pppoe_toggle_ha-main" ]; then
     cd pppoe_toggle_ha-main || exit 1
 elif [ -d "pppoe_toggle_ha" ]; then
@@ -34,25 +31,17 @@ else
 fi
 
 echo "Installing files..."
-
 cp pppoe_toggle_ha /usr/local/sbin/
 chmod 755 /usr/local/sbin/pppoe_toggle_ha
-
-if [ -f pppoe_toggle_ha.rc ]; then
-    cp pppoe_toggle_ha.rc /usr/local/etc/rc.d/pppoe_toggle_ha
-    chmod 755 /usr/local/etc/rc.d/pppoe_toggle_ha
-fi
-
-if [ -f pppoe_toggle_ha.conf ]; then
-    cp pppoe_toggle_ha.conf /usr/local/etc/devd/
-    chmod 644 /usr/local/etc/devd/pppoe_toggle_ha.conf
-fi
+cp pppoe_toggle_ha.rc /usr/local/etc/rc.d/pppoe_toggle_ha
+chmod 755 /usr/local/etc/rc.d/pppoe_toggle_ha
+cp pppoe_toggle_ha.conf /usr/local/etc/devd/
+chmod 644 /usr/local/etc/devd/pppoe_toggle_ha.conf
 
 echo "Configuring service..."
-
 sysrc -f /etc/rc.conf.local pppoe_toggle_ha_enable="YES"
-
 service pppoe_toggle_ha start
+rehash
 
 cd /
 rm -rf $TMPDIR
