332f4d37e28326b9950a2d0fa1864de8940b27e6
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-15 16:57:46 +0200

    Update command execution for Tailscale alias script

--- pppoe_toggle_ha_master.sh
+++ pppoe_toggle_ha_master.sh
@@ -3,6 +3,6 @@
 # This script runs when the node enters CARP MASTER state
 # Add any custom actions here
 
-/usr/local/scr/update_tailscale_alias
+cmd=/usr/local/scr/update_tailscale_alias; [ -x "$cmd" ] && "$cmd"
 
 exit 0
