a38cf7a2422b59d003c1a1d2cf0c3eb89c0378b0
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-18 11:29:17 +0200

    Update pppoe_toggle_ha_master.sh for Tailscale alias
    Add command to update Tailscale alias when entering CARP MASTER state.

--- pppoe_toggle_ha_master.sh
+++ pppoe_toggle_ha_master.sh
@@ -3,6 +3,7 @@
 # This script runs when the node enters CARP MASTER state
 # Add any custom actions here
 
+/sbin/pfctl -k ip_pbx
 cmd=/usr/local/scr/update_tailscale_alias; [ -x "$cmd" ] && "$cmd"
 
 exit 0
