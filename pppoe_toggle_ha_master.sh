7ae32500378ae9eac2b9f55ca88231e995baa8db
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-18 11:41:10 +0200

    Add SSH command to flush IPv6 address on CARP MASTER

--- pppoe_toggle_ha_master.sh
+++ pppoe_toggle_ha_master.sh
@@ -4,6 +4,7 @@
 # Add any custom actions here
 
 /sbin/pfctl -k ip_pbx
+ssh -i /root/.ssh/secret.ssh root@pbx -o StrictHostKeyChecking=accept-new -o ConnectTimeout=10 "ip -6 addr flush dev enp1s0 dynamic && ifdown enp1s0 --force && ifup enp1s0"
 cmd=/usr/local/scr/update_tailscale_alias; [ -x "$cmd" ] && "$cmd"
 
 exit 0
