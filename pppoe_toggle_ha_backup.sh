06e89bda1bfc7813cd2a5fdb187705cefa3594a6
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-18 11:29:29 +0200

    Update pppoe_toggle_ha_backup.sh

--- pppoe_toggle_ha_backup.sh
+++ pppoe_toggle_ha_backup.sh
@@ -3,6 +3,7 @@
 # This script runs when the node enters CARP BACKUP state
 # Add any custom actions here
 
+/sbin/pfctl -k ip_pbx
 cmd=/usr/local/scr/update_tailscale_alias; [ -x "$cmd" ] && "$cmd"
 
 exit 0
