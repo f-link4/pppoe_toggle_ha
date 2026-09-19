ad9e4928d017c0364f0a57b9c234c63cd5cf148e
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-19 18:52:17 +0200

    Add pppoe_toggle_ha.conf to uninstallation script

--- uninstall.sh
+++ uninstall.sh
@@ -24,6 +24,7 @@ FILES="
 /usr/local/sbin/pppoe_toggle_ha_backup.sh
 /usr/local/etc/rc.d/pppoe_toggle_ha
 /usr/local/etc/devd/pppoe_toggle_ha.conf
+/usr/local/etc/pppoe_toggle_ha.conf
 /tmp/pppoe_toggle_ha.state
 /tmp/pppoe_toggle_ha.cooldown
 "
