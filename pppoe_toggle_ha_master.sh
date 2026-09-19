422f644652e26e2f3ff929001083cbfa66659947
f-link4 <73019008+f-link4@users.noreply.github.com>
2026-09-19 16:06:03 +0200

    Comment out pfctl and SSH commands in script
    Comment out pfctl commands and SSH command for asterisk registration.

--- pppoe_toggle_ha_master.sh
+++ pppoe_toggle_ha_master.sh
@@ -3,10 +3,10 @@
 # This script runs when the node enters CARP MASTER state
 # Add any custom actions here
 
-/sbin/pfctl -k ip_pbx
-/sbin/pfctl -k ip_nets_voip
+#/sbin/pfctl -k ip_pbx
+#/sbin/pfctl -k ip_nets_voip
 #ssh -i /root/.ssh/secret.ssh root@pbx -o StrictHostKeyChecking=accept-new -o ConnectTimeout=10 "ip -6 addr flush dev enp1s0 dynamic && ifdown enp1s0 --force && ifup enp1s0"
-ssh -i /root/.ssh/secret.ssh root@pbx -o StrictHostKeyChecking=accept-new -o ConnectTimeout=10 "asterisk -rx 'pjsip send register *'"
+#ssh -i /root/.ssh/secret.ssh root@pbx -o StrictHostKeyChecking=accept-new -o ConnectTimeout=10 "asterisk -rx 'pjsip send register *'"
 cmd=/usr/local/scr/update_tailscale_alias; [ -x "$cmd" ] && "$cmd"
 
 exit 0
